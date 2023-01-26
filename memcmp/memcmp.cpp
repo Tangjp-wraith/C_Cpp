bfa870962eae612288da2d24962612c4c07bbf5e
Tangjp-wraith <3081129260@qq.com>
2023-01-26 17:20:28 +0800

    Update gcc Implement

--- memcmp/memcmp.cpp
+++ memcmp/memcmp.cpp
@@ -15,10 +15,23 @@ int my_memcmp(const void *buffer1, const void *buffer2, size_t count) {
   }
 }
 
+// gcc code
+int gcc_memcmp(const void *str1, const void *str2, size_t count) {
+  /**register */ const unsigned char *s1 = (const unsigned char *)str1;
+  /**register */ const unsigned char *s2 = (const unsigned char *)str2;
+  while (count-- > 0) {
+    // if中 每次都有++操作，[-1]就是访问前一个字符
+    if (*s1++ != *s2++) {
+      return s1[-1] < s2[-1] ? -1 : 1;
+    }
+  }
+  return 0;
+}
+
 int main() {
-  char s1[]="abc";
-  char s2[]="abcd";
-  int res=my_memcmp(s1,s2,4);
-  std::cout<<res<<std::endl;
+  char s1[] = "abc";
+  char s2[] = "abcd";
+  int res = gcc_memcmp(s1, s2, 4);
+  std::cout << res << std::endl;
   return 0;
 }
